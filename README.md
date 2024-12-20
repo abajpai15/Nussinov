729c6aa424c575cc129b6cb3aa7ac8e6f2a6d044
armaanbajpai <abajpai2@illinois.edu>
2024-12-19 21:02:31 -0600

    Working now

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Nussinov
+# Nussinov's Algorithm 
+
+Implementation of Nussinov's Algorithm: a nucleic acid structure prediction algorithm used in computational biology to predict the folding of an RNA molecule that makes use of dynamic programming principles. The algorithm was developed by Ruth Nussinov in the late 1970s.I
--- nussinov.py
+++ nussinov.py
@@ -3,45 +3,53 @@ def nussinov(sequence):
     dp = [[0] * n for _ in range(n)]
     bt = [[None] * n for _ in range(n)]
     
-    pair = {'A': 'U', 'U': 'A', 'C': 'G', 'G': 'C'}
+    pairs = {'A': 'U', 'U': 'A', 'C': 'G', 'G': 'C'}
     
-    for length in range(1, n):
-        for i in range(n - length):
-            j = i + length
+    for k in range(1, n):
+        for i in range(n - k):
+            j = i + k
             
-            dp[i][j] = dp[i + 1][j]
+            dp[i][j] = max(dp[i + 1][j], dp[i][j-1])
             bt[i][j] = (i + 1, j)
 
-            if pair.get(sequence[i]) == sequence[j]:
-                if dp[i + 1][j - 1] + 1 > dp[i][j]:
-                    dp[i][j] = dp[i + 1][j - 1] + 1
-                    bt[i][j] = (i + 1, j - 1)
+            if pairs[sequence[i]] == sequence[j]:
+                dp[i][j] = dp[i + 1][j - 1] + 1
+                bt[i][j] = (i + 1, j - 1)
 
-            for k in range(i, j):
-                if dp[i][k] + dp[k + 1][j] > dp[i][j]:
-                    dp[i][j] = dp[i][k] + dp[k + 1][j]
-                    bt[i][j] = (i, k, k + 1, j)
-    
-    def backtrace(i, j, structure):
-        if i >= j:
-            return
-        
-        if bt[i][j] is None:
-            return
+            for x in range(i, j):
+                if dp[i][x] + dp[x + 1][j] > dp[i][j]:
+                    dp[i][j] = dp[i][x] + dp[x + 1][j]
+                    bt[i][j] = (i, x, x + 1, j)
 
-        if len(bt[i][j]) == 2:
-            ni, nj = bt[i][j]
-            structure[i] = '('
-            structure[j] = ')'
-            backtrace(ni, nj, structure)
-        elif len(bt[i][j]) == 4:
-            i1, j1, i2, j2 = bt[i][j]
-            backtrace(i1, j1, structure)
-            backtrace(i2, j2, structure)
-        else:
-            ni, nj = bt[i][j]
-            backtrace(ni, nj, structure)
+    soln = traceback(sequence, dp)
 
     structure = ['.'] * n
-    backtrace(0, n - 1, structure)
-    return dp[0][n - 1], ''.join(structure)
+    for (i, j) in soln:
+        structure[i] = '('
+        structure[j] = ')'
+    return dp[0][-1], ''.join(structure)
+
+
+def traceback(sequence, dp):
+    n = len(sequence)
+    stack = [(0,n-1)]
+    soln = []
+    while len(stack) > 0:
+        i,j = stack.pop()
+        if i >= j:
+            continue
+        elif dp[i+1][j] == dp[i][j]:
+            stack.append((i+1, j))
+        elif dp[i][j-1] == dp[i][j]:
+            stack.append((i, j-1))
+        elif dp[i+1][j-1] +1 == dp[i][j]:
+            soln.append((i, j))
+            stack.append((i+1, j-1))
+        else:
+            for k in range(i+1, j):
+                if dp[i][k] + dp[k+1][j] == dp[i][j]:
+                    stack.append((k+1, j))
+                    stack.append((i, k))
+                    break
+
+    return soln
